97d9d5a08e9b6b4f27079f67e959ac854864fd45
omololaowonikoko <omololaowonikoko@gmail.com>
2024-10-10 13:22:47 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from gettext import install
+from locale import currency
 
 exchange_rate = 3.81
 pln_calculated = int(input("Enter amount in USD: "))
@@ -63,10 +64,10 @@ exchange_rate_calculated = exchange_amount * data_2
 print(f"Amount of {user_input1.upper()}/ {user_input2.upper()}  for {exchange_amount} {user_input1.upper()}= {exchange_rate_calculated} {user_input2.upper()}")
 
 while True:
-    ask_user = input("Do you wish to make another exchange rate? (y/n): ")
-    if ask_user == "n":
+    ask_user = input("Do you wish to make another exchange rate? (y/n): ").lower()
+    if ask_user == "n".lower():
         break
-    elif ask_user == "y":
+    elif ask_user == "y".lower():
         exchange_amount = int(input("Enter amount for exchange: "))
         user_input1 = input("Enter first CCY: ")
         user_input2 = input("Enter second CCY: ")
@@ -74,12 +75,27 @@ while True:
         pair_realtime_rates = requests.get(new_url)
         data_2 = pair_realtime_rates.json()
         print(json.dumps(data_2, indent=7))
-        for key, value in data_2.items():
-            data_2 = value
-            print(data_2)
-            exchange_rate_calculated = exchange_amount * data_2
-            print(f"Amount of {user_input1.upper()}/ {user_input2.upper()}  for {exchange_amount} {user_input1.upper()}= {exchange_rate_calculated} {user_input2.upper()}")
-
+        data = data_2.get("conversion_rate")
+        print(data)
+        exchange_rate_calculated = exchange_amount * data
+        print(f"Amount of {user_input1.upper()}/ {user_input2.upper()}  for {exchange_amount} {user_input1.upper()}= {exchange_rate_calculated} {user_input2.upper()}")
+#evolution 4
+user_input1 = input("Enter currency amount and first CCY: e.g 10000 USD: ")
+currency_amount, first_ccy = user_input1.split(" ")
+currency_amount = float(currency_amount)
+first_ccy =str(first_ccy).upper()
+print(first_ccy, currency_amount)
+user_input2 = input("Enter second CCY: ")
+new_url = (f"https://v6.exchangerate-api.com/v6/2d3a8f713c3474eb9c072a8f/pair/{first_ccy}/{user_input2}")
+print(new_url)
+pair_realtime_rates = requests.get(new_url)
+data_2 = pair_realtime_rates.json()
+print(json.dumps(data_2, indent=7))
+data = data_2.get("conversion_rate")
+print(data)
+exchange_rate_calculated = round(currency_amount * data, 4)
+print(exchange_rate_calculated)
+print(f"Amount of {first_ccy.upper()}/ {user_input2.upper()}  for {currency_amount} {first_ccy.upper()}= {exchange_rate_calculated} {user_input2.upper()}")
 
 
 

e42bed2b288887efa0b11677ac255c2fbfcab4e0
omololaowonikoko <omololaowonikoko@gmail.com>
2024-10-22 12:58:13 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from fileinput import filename
 
 import requests
 import json
 
-from main_2v import ask_user
+
 def get_user_input(user_input1):
     currency_amount, first_ccy = user_input1.split(" ")
     return float(currency_amount), first_ccy.upper()
@@ -22,20 +23,46 @@ def exchange_rate_calculated(data, currency_amount):
 def print_result(first_ccy,second_ccy,exchange_rate_calculated,currency_amount):
     return print(f"Amount of {first_ccy.upper()}/ {second_ccy.upper()}  for {currency_amount} {first_ccy.upper()}= {exchange_rate_calculated} {second_ccy.upper()}")
 
+def save_result_to_file(filename,first_ccy,second_ccy,conversion_rate,currency_amount,exchange_rate_calculated):
+    with open(filename, 'a') as file:
+        file.write(f"{'First Currency':<15}{'Second Currency':<15}{'Conversion Rate':<20}{'Currency Amount':<25}{'Calculated Amount':<20}\n")
+        file.write(f"{'-' * 15}{'-' * 15}{'-' * 20}{'-' * 25}{'-' * 20}\n")
+
+            #To append
+        file.write(f"{first_ccy:<15}{second_ccy:<15}{conversion_rate:<20}{exchange_rate_calculated:<25}{currency_amount:<20}\n")
+        print(f"Exchange result has been appended to {filename}")
+
+
+
+
 if __name__ == "__main__":
-    try:
-        user_input1 = input("Enter currency amount and first CCY: e.g 10000 USD: ")
-        currency_amount, first_ccy = get_user_input(user_input1)
+    filename = "Currencies_Exchange_results.txt"
+    while True:
+        try:
+            ask_user = (input("Do you wish to make an exchange? (y/n): "))
+            if ask_user == "n":
+                break
+            elif ask_user == "y":
+                user_input1 = input("Enter currency amount and first CCY: e.g 10000 USD: ")
+                currency_amount, first_ccy = get_user_input(user_input1)
 
 
-        user_input2 = input("Enter second CCY: ")
-        second_ccy = get_user_input_2(user_input2)
+                user_input2 = input("Enter second CCY: ")
+                second_ccy = get_user_input_2(user_input2)
 
-        exchange_rates = get_exchange_rates(first_ccy, second_ccy)
-        exchange_amount = exchange_rate_calculated(exchange_rates, currency_amount)
-        print_result(first_ccy,second_ccy,exchange_rates,currency_amount)
+                exchange_rates = get_exchange_rates(first_ccy, second_ccy)
+                exchange_amount = exchange_rate_calculated(exchange_rates, currency_amount)
+                print_result(first_ccy,second_ccy,exchange_rates,currency_amount)
 
-    except ValueError as e:
-        print(f"Invalid input: {e}")
-    except requests.exceptions.RequestException as e:
-        print(f"Error with the request: {e}")
+                save_result_to_file(filename, first_ccy, second_ccy,exchange_rates,currency_amount,exchange_rate_calculated)
+
+        except ValueError as e:
+            print(f"Invalid input: {e}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error with the request: {e}")
+
+
+
+import unittest
+
+
